5577500c4c50ad43f1f982bcaea751b1a8e6e111
Scott Kuhl <kuhl@mtu.edu>
2012-04-30 19:38:40 -0400

    fix environment variable bug

--- testdocs/prepost/prepost.sh
+++ testdocs/prepost/prepost.sh
@@ -2,8 +2,10 @@
 
 if [[ $1 == "pre" ]]; then
     echo "prepost.sh: Running script before document"
-fi
-
-if [[ $1 == "post" ]]; then
+elif [[ $1 == "post" ]]; then
     echo "prepost.sh: Running script after document"
+elif [[ $1 == "interrupt" ]]; then
+    echo "prepost.sh: Running script because we were interrupted"
+else
+    echo "prepost.sh: Unhandled parameter $1"
 fi
