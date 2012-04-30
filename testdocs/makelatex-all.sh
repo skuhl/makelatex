fa64edceeb9a2faa1195a948b4f784f654164796
Scott Kuhl <kuhl@mtu.edu>
2012-04-29 23:12:28 -0400

    Modify makelatex-all script to better handle Ctrl+C interruptions. Also, make it quieter.

--- testdocs/makelatex-all.sh
+++ testdocs/makelatex-all.sh
@@ -2,12 +2,16 @@
 
 PATH="../..:$PATH"
 
+# kill the script entirely if ctrl+c is pressed
+trap ctrl_c INT
+function ctrl_c() { exit; }
+
 for i in *; do
     if [[ -d $i ]]; then
         echo
         echo "Working in $i"
         cd $i
-        makelatex -o -f --screen --greyscale --nupHandout --nupPort 2x1
+        makelatex -q -o -f --screen --greyscale --nupHandout --nupPort 2x1
         echo "makelatex returned status code: $?"
         cd ..
     fi
